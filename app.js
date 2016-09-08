c81dbc9a45683c973f6383f00643936ff55f4bd7
shivabpatil <shivanandpatil@live.in>
2016-09-08 23:24:41 +0530

    added body-parser and post method for admin,sercive center,customers and bike

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 
+var bodyParser = require('body-parser');
+
 //databse connection 
 var db = mongoose.connect('mongodb://localhost:27017/pnddata');
 
@@ -15,6 +17,10 @@ var app = express();
 
 var port = process.env.PORT | 4000;
 
+// add body-parsr to app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //declare routers for colections 
 var adminRouter = express.Router();
 var servicecenterRouter = express.Router();
@@ -29,52 +35,63 @@ app.use('/api',bikeRouter);
 
 //admin aoi routes 
 
-adminRouter.route('/admins').get(function (req,res) {
-
-	//query with name
-	var query={};
-	if (req.query.name) {
-	 	query.name=req.query.name;
-	 } 
-
-	//return  all admins
-	Admin.find(query,function (error,admins) {
-		if (error) {
-			res.status(500).send(error);
-		} else {
-			res.json(admins);
-		}
+adminRouter.route('/admins')
+	.post(function (req,res) {
+		var admin = new Admin(req.body); 
+		console.log(admin);
+		res.send(admin);
+	})
+	.get(function (req,res) {
+		//query with name
+		var query={};
+		if (req.query.name) {
+		 	query.name=req.query.name;
+		 } 
+		//return  all admins
+		Admin.find(query,function (error,admins) {
+			if (error) {
+				res.status(500).send(error);
+			} else {
+				res.json(admins);
+			}
+		});
 	});
-});
 
 // get single admin 
-adminRouter.route('/admins/:adminId').get(function (req,res) {
-	Admin.findById(req.params.adminId,function (error,admin) {
-		if (error) {
-		 	res.status(500).send(error);
-		 } else {
-		 	res.json(admin);
-		 } 
+adminRouter.route('/admins/:adminId')
+	.get(function (req,res) {
+		Admin.findById(req.params.adminId,function (error,admin) {
+			if (error) {
+			 	res.status(500).send(error);
+			 } else {
+			 	res.json(admin);
+			 } 
+		});
 	});
-});
 
 //service center api routes
-servicecenterRouter.route('/servicecenters').get(function (req,res) {
-	// query wiht name 
-	var query={}
-	if (req.query.name) {
-		query.name=req.query.name;
-	}
-
-	//return all service center
-	Servicecenter.find(query,function (error,servicecenters) {
-		if (error) {
-		 	res.status(500).send(error);
-		 } else {
-		 	res.json(servicecenters);
-		 } 
+servicecenterRouter.route('/servicecenters')
+	.post(function (req,res) {
+		var servicecenter = new Servicecenter(req.body);
+		console.log(servicecenter);
+		res.send(servicecenter); 
 	})
-})
+	.get(function (req,res) {
+		// query wiht name 
+		var query={}
+		if (req.query.name) {
+			query.name=req.query.name;
+		}
+
+		//return all service center
+		Servicecenter.find(query,function (error,servicecenters) {
+			if (error) {
+			 	res.status(500).send(error);
+			 } else {
+			 	res.json(servicecenters);
+			 } 
+		})
+	})
 
 //get single service center
 
@@ -90,23 +107,29 @@ servicecenterRouter.route('/servicecenters/:servicecenterId')
 	});
 
 //customer api routes 
-customerRouter.route('/customers').get(function (req,res) {
-
-	//query with contact number 
-	var query={};
-	if (req.query.contact) {
-		query.contact=req.query.contact;
-	}
-
-	//return all customers 
-	Customer.find(query,function (error,customers) {
-		if (error) {
-		 	res.staus(500).send(error);
-		 } else {
-		 	res.json(customers);
-		 } 
+customerRouter.route('/customers')
+	.post(function (req,res) {
+		var customer = new Customer(req.body);
+		console.log(customer);
+		res.send(customer); 
 	})
-});
+	.get(function (req,res) {
+
+		//query with contact number 
+		var query={};
+		if (req.query.contact) {
+			query.contact=req.query.contact;
+		}
+
+		//return all customers 
+		Customer.find(query,function (error,customers) {
+			if (error) {
+			 	res.staus(500).send(error);
+			 } else {
+			 	res.json(customers);
+			 } 
+		})
+	});
 
 //get single customer 
 customerRouter.route('/customers/:customerId')
@@ -122,16 +145,22 @@ customerRouter.route('/customers/:customerId')
 
 //bike api routes
 
-bikeRouter.route('/bikes').get(function (req,res) {
-	// return all bikes
-	Bike.find(function (error,bikes) {
-		if (error) {
-		 	res.status(500).send(error);
-		 } else {
-		 	res.json(bikes);
-		 } 
+bikeRouter.route('/bikes')
+	.post(function (req,res) {
+		var bike = new Bike(req.body);
+		console.log(bike);
+		res.send(bike); 
+	})
+	.get(function (req,res) {
+		// return all bikes
+		Bike.find(function (error,bikes) {
+			if (error) {
+			 	res.status(500).send(error);
+			 } else {
+			 	res.json(bikes);
+			 } 
+		});
 	});
-});
 
 bikeRouter.route('/bikes/:bikeId')
 	.get(function (req,res) {
