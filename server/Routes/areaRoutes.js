d367cc7916979974959ce4cf606cc1cf2b5e9a08
shivabpatil <shivanandpatil@live.in>
2016-12-14 14:56:16 +0530

    Adde API for new area table

--- server/Routes/areaRoutes.js
+++ server/Routes/areaRoutes.js
@@ -30,11 +30,25 @@ var routes = function (Area) {
 		})
 		.put(function (req,res) {
 			req.area.name = req.body.name;
-			req.area.no_service_centers = req.body.no_service_centers;
-			req.area.no_dpersons = req.body.no_dpersons;
-			req.area.service_center_id = req.body.service_center_id;
-			req.area.dperson_id = req.body.dperson_id;
-			
+			req.area.total_service_centers = req.body.total_service_centers;
+			req.area.total_dpersons = req.body.total_dpersons;	
+			for(var serviceCenter in req.body.service_centers)
+				req.area.service_centers.push({
+					name:req.body.service_centers[serviceCenter].name,
+					brand:req.body.service_centers[serviceCenter].brand,
+					email:req.body.service_centers[serviceCenter].email,
+					contact:req.body.service_centers[serviceCenter].contact,
+					address:req.body.service_centers[serviceCenter].address,
+					capacity:req.body.service_centers[serviceCenter].capacity,
+					start_time:req.body.service_centers[serviceCenter].start_time,
+					end_time:req.body.service_centers[serviceCenter].end_time
+				})
+			for(var slot in req.body.slots)
+				req.area.slots.push({
+					slot_number:req.body.slots[slot].slot_number,
+					slot_time:req.body.slots[slot].slot_time,
+					slot_capacity:req.body.slots[slot].slot_capacity,		
+				})
 			req.area.save(function (error) {
 				if (error) {
 					res.status(500).send(error);
--- server/models/areaModel.js
+++ server/models/areaModel.js
@@ -6,17 +6,69 @@ var areaModel = new Schema({
 		type:String,
 		required:true
 	},
-	no_service_centers:{
+
+	total_service_centers:{
+		type:Number,
+		default:0,
+	},
+
+	total_dpersons:{
 		type:Number,
 	},
-	no_dpersons:{
-		type:Number,
-	},
-
-	service_center_id:[String],
-
-	dperson_id:[String],
-
+    
+    service_centers:[ {
+    	name:{
+			type:String,
+		},
+		brand:{
+			type:String,
+		},
+		email:{
+			type:String,
+		},
+		contact:{
+			type:Number,
+		},
+		address:{
+			line1:{
+				type:String,
+			},
+			landmark:{
+				type:String,
+			},
+			area:{
+				type:String,
+			},
+			city:{
+				type:String,
+				default:"Pune"
+			},
+			state:{
+				type:String,
+				default:"Maharashtra"
+			}
+		},
+		capacity:{
+			type:Number,
+		},	
+		start_time:{
+			type:String
+		},
+		end_time:{
+			type:String
+		},
+    }],
+    slots:[{
+    	slot_number:{
+			type:Number,
+		},
+    	slot_time:{
+			type:String,
+		},
+		slot_capacity:{
+			type:Number,
+		},
+    }],
 	create_at:{
 		type:Date,
 		default: Date.now
