319e3deefa9132277745f9bcc71881afb1cd173a
shivabpatil <shivanandpatil@live.in>
2016-12-20 10:42:21 +0530

    Added path for the pages

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,10 @@ angular.module('pndApp').config(function($routeProvider,$locationProvider) {
 	$locationProvider.html5Mode(true);
 	$routeProvider
 		.when('/',{templateUrl:'partials/main/main',controller:'pndMainCtrl'})
+		.when('/about',{templateUrl:'partials/main/about',controller:'pndMainCtrl'})
+		.when('/blog',{templateUrl:'partials/main/blog',controller:'pndMainCtrl'})
+		.when('/feedback',{templateUrl:'partials/main/feedback',controller:'pndMainCtrl'})
+		.when('/annual_packages',{templateUrl:'partials/main/annualPackages',controller:'pndMainCtrl'})
 		.when('/customer',{templateUrl:'partials/customer/create',controller:'pndCustomerCtrl'})
 		.when('/schedules',{templateUrl:'partials/schedule/index',controller:'pndScheduleCtrl'})
 		.when('/areas',{templateUrl:'partials/area/create',controller:'pndAreaCtrl'})
@@ -15,6 +19,3 @@ angular.module('pndApp').config(function($routeProvider,$locationProvider) {
 		.when('/serviceCenters',{templateUrl:'partials/area/createServiceCenter',controller:'pndServiceCenterCtrl'})
 		.when('/slots',{templateUrl:'partials/area/createSlot',controller:'pndSlotCtrl'})
 })
-
-
-
