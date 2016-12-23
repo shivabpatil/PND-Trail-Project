f411f196970c47bdd1ffc33fa86622c8e911f74a
shivabpatil <shivanandpatil@live.in>
2016-12-23 18:00:17 +0530

    Added the validation for slot form

--- public/app/area/pndSlotCtrl.js
+++ public/app/area/pndSlotCtrl.js
@@ -1,7 +1,7 @@
 angular.module('pndApp').controller('pndSlotCtrl', function($scope,areaService,$location,$route,$filter,$window,$rootScope){
 
 	// get the list of the Areas
-    $scope.options = ['6-7','7-8','8-9','9-10','10-11','11-12','12-13','13-14','14-15','15-16','16-17','17-18','18-19','19-20','20-21']
+  $scope.options = ['6-7','7-8','8-9','9-10','10-11','11-12','12-13','13-14','14-15','15-16','16-17','17-18','18-19','19-20','20-21']
 	areaService.getAreas().then(function(res){
 		$scope.areas = res.data;
 		console.log("$scope.areas");
@@ -20,7 +20,7 @@ angular.module('pndApp').controller('pndSlotCtrl', function($scope,areaService,$
 
 	//create the area
 
-	$scope.create = function (area,areaSelected) {
+	$scope.create = function (area,areaSelected,slotForm) {
 		//delete area._id;
 		console.log(areaSelected);
 		console.log(area);
@@ -29,8 +29,11 @@ angular.module('pndApp').controller('pndSlotCtrl', function($scope,areaService,$
 		area.total_service_centers = areaSelected.total_service_centers;
 		area.total_dpersons = areaSelected.total_dpersons;
 		area.service_centers = [];
-		areaService.editArea(areaSelected._id,area);
-	    $route.reload();
+    if(slotForm.$valid){
+      areaService.editArea(areaSelected._id,area);
+  	  $route.reload();
+    }
+
 	}
 
 	// //delete the area
